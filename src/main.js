967cd95ef1400cb09b534c84d84431004424821a
HowInteresting <qsceszadwx11@163.com>
2025-10-20 15:46:39 +0000

    Deploying to gh-pages from @ HowInteresting/howinteresting.github.io@b4438c6abf1a576be24e2bb77f5ae827befbbca9 🚀

---
+++ src/main.js
@@ -0,0 +1,38 @@
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import "@/style/style.scss";
+import Home from "@/pages/home.vue";
+import Love from "@/pages/love.vue";
+import Birth from "@/pages/birth.vue";
+import App from "@/App.vue";
+// 引入 pinia
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+// swiper
+import "swiper/css";
+
+const app = createApp(App);
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Home },
+    { path: "/home", component: Home },
+    { path: "/love", component: Love },
+    { path: "/birthday", component: Birth },
+    { path: "/*", component: Home },
+  ],
+});
+
+app.use(pinia);
+app.use(router);
+app.mount("#app");
+
+// PWA
+navigator.serviceWorker.addEventListener("controllerchange", () => {
+  // 弹出更新提醒
+  console.log("站点已更新，刷新后生效");
+  ElMessage("站点已更新，刷新后生效");
+});
