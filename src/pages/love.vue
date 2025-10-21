739d9fddfc9286f7bc03813848139f46157389af
lixr <lixr@cqnews.net>
2025-10-21 09:06:04 +0800

    init

--- src/pages/love.vue
+++ src/pages/love.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe width="100%" height="100%" frameborder="0" src="/love/index.html"></iframe>
+  <iframe width="100vw" height="100vh" frameborder="0" src="/love/index.html"></iframe>
 </template>
 
 <script setup></script>
