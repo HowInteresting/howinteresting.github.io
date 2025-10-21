3abea198fb3f3f198ddb0d5dffd3711d885a9c35
HowInteresting <qsceszadwx11@163.com>
2025-10-21 01:08:29 +0000

    Deploying to gh-pages from @ HowInteresting/howinteresting.github.io@739d9fddfc9286f7bc03813848139f46157389af 🚀

--- src/pages/birth.vue
+++ src/pages/birth.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe width="100%" height="100%" frameborder="0" src="/birth/index.html"></iframe>
+  <iframe width="100vw" height="100vh" frameborder="0" src="/birth/index.html"></iframe>
 </template>
 
 <script setup></script>
