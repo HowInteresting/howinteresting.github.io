0e68cf8cbd3606a3dbe3551d9ef3a4e6bec2e74c
HowInteresting <qsceszadwx11@163.com>
2025-10-27 08:21:52 +0000

    Deploying to gh-pages from @ HowInteresting/howinteresting.github.io@8fef55d7b95787e7afa89a5e404a9358f518f9fb 🚀

--- public/birthday/js/index.js
+++ public/birthday/js/index.js
@@ -22,7 +22,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate("|#countdown 3||WY|祝你|生日|快乐|🎂|#time");
+      S.UI.simulate("|#countdown 3||WY|祝你|生日|快乐|🎂|I♥U|#time");
     }
 
     S.Drawing.loop(function () {
